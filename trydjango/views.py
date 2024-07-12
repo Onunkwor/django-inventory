bbf91e439ebe376f79ff5e2e32590ee45502d752
raphael <you@example.com>
2024-07-12 13:56:56 +0100

    first commit

---
+++ trydjango/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from articles.models import Article
+from django.template.loader import render_to_string
+
+def home_view(request):
+    article_obj = Article.objects.get(id=4)
+    context = {
+        "title": article_obj.title,
+         "content": article_obj.content
+    }
+   
+    HTML_STRING = render_to_string('home-view.html', context=context)
+    return HttpResponse(HTML_STRING)
