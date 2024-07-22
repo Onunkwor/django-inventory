bd74c512ffadd298c677d1046958752e311e270a
raphael <you@example.com>
2024-07-22 11:48:59 +0100

    moved slugify function to utils file

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,13 @@
+import random
 from django.db import models
-from django.utils.text import slugify
 from django.db.models.signals import pre_save, post_save
+from articles.utils import slugify_instance_title
 # Create your models here.
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -16,14 +17,6 @@ class Article(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-
-
-def slugify_instance_title(instance, save=False):
-    slug = slugify(instance.title)
-    instance.slug = slug
-    if save:
-        instance.save()
-    return instance
 
 
 def article_pre_save(sender, instance, *args, **kwargs):
@@ -38,7 +31,7 @@ pre_save.connect(article_pre_save, sender=Article)
 def article_post_save(sender, instance, created, *args, **kwargs):
     print('post_save')
     if created:
-        slug = slugify_instance_title(instance)
+        slug = slugify_instance_title(instance, save=True)
 
 
 post_save.connect(article_post_save, sender=Article)
